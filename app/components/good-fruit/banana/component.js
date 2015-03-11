76f8d157e9a49fcecf3fe501e514f55d1494d574
James Murphy <jamesmurphyau@me.com>
2015-03-11 12:07:08 +1100

    Workaround

---
+++ app/components/good-fruit/banana/component.js
@@ -0,0 +1,8 @@
+import FruitComponent from '../component';
+import template from './component-template';
+import layout from '../template';
+
+export default FruitComponent.extend({
+  layout: layout,
+  template: template
+});
